56c632eaad64f8de8e53892e6a19ad756e7e312a
Shaludnev D <71229316+lxgf@users.noreply.github.com>
2022-03-18 14:34:25 +0600

    Some fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import Main from "./FiltersAndMasks/components/Main";
+import Main from "./FiltersAndMarks/components/Main";
 
 ReactDOM.render(
   <React.StrictMode>
